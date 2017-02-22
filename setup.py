ae7ba28d9bc490019d13076ab0a7f5e03ffdcbe1
Richard Fan <richardfan1126@gmail.com>
2017-02-22 18:40:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,14 +23,10 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='PyAlgoTrade',
-    version='0.18',
+    name='BuzzPoemAlgoTrade',
+    version='0.01',
     description='Python Algorithmic Trading',
     long_description='Python library for backtesting stock trading strategies.',
-    author='Gabriel Martin Becedillas Ruiz',
-    author_email='pyalgotrade@gmail.com',
-    url='http://gbeced.github.io/pyalgotrade/',
-    download_url='http://sourceforge.net/projects/pyalgotrade/files/0.18/PyAlgoTrade-0.18.tar.gz/download',
     packages=[
         'pyalgotrade',
         'pyalgotrade.barfeed',
